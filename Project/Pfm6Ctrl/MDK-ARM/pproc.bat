964df3742cb5264e6517b4bb5b05565af96c8c58
MOCNIK <matjaz.mocnik@fotona.com>
2020-10-08 14:27:50 +0200

    * v 7.01 Oct  6 2020, <58EA9BED> - SYS timing error not critical - SYS error fibre also calls simmer off - image, cfg.ini changed - bin file generator, pproc.bat modified

--- Project/Pfm6Ctrl/MDK-ARM/pproc.bat
+++ Project/Pfm6Ctrl/MDK-ARM/pproc.bat
@@ -1,2 +1,3 @@
-del   ..\..\..\bin\*.hex
+del   ..\..\..\bin\*.bin, ..\..\..\bin\*.hex
 copy  output\*.hex  ..\..\..\bin\*.hex
+srec_cat.exe ..\..\..\bin\Pfm8Ctrl.hex -Intel -crop 0x8008000 -offset -0x8008000 -o ..\..\..\bin\Pfm8Ctrl.bin -Binary
